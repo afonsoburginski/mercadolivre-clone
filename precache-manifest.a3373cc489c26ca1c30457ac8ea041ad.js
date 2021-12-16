c7d01cb50a5044b1cab19f596679e41d3d70705e
Afonso Kevin Burginski <83406890+afonsoburginski@users.noreply.github.com>
2021-12-16 01:22:23 -0300

    github pages

---
+++ precache-manifest.a3373cc489c26ca1c30457ac8ea041ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "821609f420c76c32d22ac12e7d52340b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "134f83462f58a60559bf",
+    "url": "./static/js/2.4766a072.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.4766a072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "377f1d6085472a09a6cc",
+    "url": "./static/js/main.b56d6628.chunk.js"
+  },
+  {
+    "revision": "539595d712ecdcf96759",
+    "url": "./static/js/runtime-main.9dadc82f.js"
+  },
+  {
+    "revision": "7df03694fbb128e2b06e30c2f883f42e",
+    "url": "./static/media/tshirt.7df03694.png"
+  }
+]);
--- src/components/Header/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { Container } from "./styles";
-
-const Header: React.FC = () => {
-  return <Container>Mercado Livre - UI Clone</Container>;
-};
-
-export default Header;
